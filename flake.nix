d16b88c6879b302fa8f3977e46423bf5ce552175
hitsmaxft <mfthits@gmail.com>
2025-01-27 21:33:36 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "flake rmk with rust toolchain";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    let
+      systemBuildInputs = system: pkgs:
+        [ pkgs.iconv pkgs.openssl ]
+        ++ (if pkgs.lib.strings.hasInfix "$system" "darwin" then [
+          pkgs.darwin.apple_sdk.frameworks.Security
+          pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+        ] else
+          [ ]);
+    in utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        # defaultPackage = naersk-lib.buildPackage {
+        #   src = ./.;
+        #   buildInputs = (systemBuildInputs system pkgs).${system};
+        # };
+
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              rustup
+              rustfmt
+              probe-rs-tools
+              # pre-commit
+              # rustPackages.clippy
+            ] ++ (systemBuildInputs system pkgs);
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+      });
+}
